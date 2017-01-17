46d80802f2ca5455f58a2ac3df3afe9064a49ab7
Dangle Nuño <dangle@users.noreply.github.com>
2017-01-16 21:25:38 -0700

    Add --debug and --verbose CLI arguments.

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -3,6 +3,44 @@
 
 from __future__ import unicode_literals
 
+import re
+
+
+_CTRL_CHAR = re.compile(r'(\x9B|\x1B\[)[0-?]*[ -/]*[@-~]')
+_LOG = re.compile(
+    r'(?P<level>[^\s]+)\s+\[.+\]\[(?P<logger>.+?)\]\s+(?P<log>.+)')
+
+
+def test_log_levels(create_project, run):
+    """Test that an invalid log level does not show an exception."""
+    def _get_logs(command):
+        output = _CTRL_CHAR.sub('', run(command, stderr=True))
+        return [(m[0], m[2]) for m in _LOG.findall(output) if m[1] == 'root']
+
+    with create_project('''
+        import logging
+        def log():
+            """usage: say log"""
+            logging.debug('DEBUG')
+            logging.info('INFO')
+            logging.warn('WARN')
+            logging.error('ERROR')
+    '''):
+        expected_logs = [
+            ('DEBUG', 'DEBUG'),
+            ('INFO', 'INFO'),
+            ('WARNING', 'WARN'),
+            ('ERROR', 'ERROR'),
+        ]
+        assert _get_logs('say --d log') == expected_logs
+        assert _get_logs('say --debug log') == expected_logs
+        assert _get_logs('say --log-level DEBUG log') == expected_logs
+        assert _get_logs('say -v log') == expected_logs[1:]
+        assert _get_logs('say --verbose log') == expected_logs[1:]
+        assert _get_logs('say --log-level INFO log') == expected_logs[1:]
+        assert _get_logs('say --log-level WARN log') == expected_logs[2:]
+        assert _get_logs('say --log-level ERROR log') == expected_logs[3:]
+
 
 def test_invalid_log_level(create_project, run):
     """Test that an invalid log level does not show an exception."""
